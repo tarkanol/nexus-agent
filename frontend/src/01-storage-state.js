54fb3715406dc55872d39713256aec2879f21a31
tarkanol <tarkanol@gmail.com>
2026-07-14 22:39:21 +0300

    Update 01-storage-state.js

--- frontend/src/01-storage-state.js
+++ frontend/src/01-storage-state.js
@@ -9,6 +9,7 @@
     removeItem: function(k) { delete memory[k]; }
   };
   try {
+    
     var nativeStore = window.localStorage;
     var probe = '__apex_storage_probe__';
     nativeStore.setItem(probe, '1');
